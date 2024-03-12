7198c7cc2e7069b890d2e8779bfd107b464d36cc
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-03-12 19:25:15 -0300

    Update SearchResult.js to insert details

--- frontend/src/routes/search/SearchResult.js
+++ frontend/src/routes/search/SearchResult.js
@@ -1,20 +1,45 @@
-import React from "react"
-import { useState } from "react"
-import { Link } from "react-router-dom";
-import '../../style/SearchResult.css'
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import '../../style/Restaurants.css';
 
 const API_BASE = "http://localhost:3001"
 
 const SearchResult = () => {
-    const [restaurants] = useState([]);
-    const [error] = useState(null);
+    const [restaurants, setSearchResult] = useState([]);
+    const [error, setError] = useState(null);
+    const location = useLocation();
+    const { restaurantName } = location.state;
+
+    useEffect(() => {
+        const GetSearchResult = async () => {
+            try {
+                const response = await fetch(`/searches/search_restaurant?name=${encodeURIComponent(restaurantName)}`, {
+                    method: "GET",
+                });                
+
+                if (!response.ok) {
+                    throw new Error("Failed to fetch search results");
+                }
+
+                const data = await response.json();
+                setSearchResult(data); // Corrected function to match the state setter name
+            } catch (error) {
+                console.error("Error fetching search results:", error);
+                setError(error.message);
+            }
+        };
+
+        if (restaurantName) {
+            GetSearchResult();
+        }
+    }, [restaurantName]); 
 
     return (
         <div>
 
             <div className="restaurants-page"> 
                 <Link className="link" to={`/restaurants/create`}>
-                    <div className="create-button">
+                    <div className="simple-button" id="create-button">
                         <p>Cadastrar restaurante</p>
                     </div>
                 </Link>
@@ -22,7 +47,7 @@ const SearchResult = () => {
                 <div className="restaurant-list"> 
                     <h2 className="category"> Resultado da Busca </h2>
                     {restaurants.length === 0 && (
-                        <h1 className= "noData">AINDA NÃO HÁ RESTAURANTES CADASTRADOS</h1>
+                        <h1 className= "noDataRestaurant">NENHUM RESTAURANTE FOI ENCONTRADO</h1>
                     )}
                     {restaurants.map(restaurant => (
                         <div className="restaurant-preview" key={restaurant.id}> 
