a66ebd9cd367d74987807f7f651bbb0dcb79c9eb
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-03-12 11:48:14 -0300

    Update Feed.js to include more content

--- frontend/src/routes/feed/Feed.js
+++ frontend/src/routes/feed/Feed.js
@@ -1,56 +1,93 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom";
 import '../../style/Feed.css'
-import triangle from "../../assets/triangle.png";
+import Header from "../../header/Header.js";
 const API_BASE = "http://localhost:3001"
 
 const Feed = () => {
-    const [restaurants] = useState([]);
-    const [reviews] = useState([]);
+    const [restaurants, setRandomRestaurants] = useState([])
+    const [reviews, setRandomReviews] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        GetRandomRestaurants()
+            .catch(err => {
+                console.error("Error: ", err);
+                setError(err.message);
+            });
+
+        GetRandomReviews()
+            .catch(err => {
+                console.error("Error: ", err);
+                setError(err.message);
+            });
+    }, []); 
+
+    const GetRandomRestaurants = async () => {
+        const response = await fetch(API_BASE + "/feed/random_restaurants");
+        if (!response.ok) {
+            throw new Error("Failed to fetch");
+        }
+        const data = await response.json();
+        setRandomRestaurants(data);
+    };
+
+    const GetRandomReviews = async () => {
+        const response = await fetch(API_BASE + "/feed/random_reviews");
+        if (!response.ok) {
+            throw new Error("Failed to fetch");
+        }
+        const data = await response.json();
+        setRandomReviews(data);
+    };
 
     return (
         <div>
+            <div className="header">
+                <Header />
+            </div>
 
-            <h1 className="title">Reviews em Alta</h1>
+            <h1 className="titleFeed">Reviews em Alta</h1>
             {reviews.length === 0 && (
-                <div className="content">
-                    <h1 className= "noDataReview">AINDA NÃO EXISTEM REVIEWS CADASTRADAS NO SITE</h1>
+                <div className="noContentFeed">
+                    <h1 className= "noDataReviewFeed">AINDA NÃO EXISTEM REVIEWS CADASTRADAS NO SITE</h1>
                 </div>
             )}
             {reviews.length > 0 && (
-                <div className="content">
-                    <img src={triangle} alt="triangle" className="leftTriangle"/>
-                    <img src={triangle} alt="triangle" className="rightTriangle"/>
+                <div className="contentFeed">
+                    
                 </div>
-            )}
+            )}                               
 
-            <h1 className="title">Restaurantes</h1>
+            <h1 className="titleFeed">Restaurantes</h1>
             {restaurants.length === 0 && (
-                <div className="content">
-                    <h1 className= "noDataRestaurant">AINDA NÃO EXISTEM RESTAURANTES CADASTRADOS NO SITE</h1>
+                <div className="noContentFeed">
+                    <h1 className= "noDataRestaurantFeed">AINDA NÃO EXISTEM RESTAURANTES CADASTRADOS NO SITE</h1>
                 </div>
             )}
             {restaurants.length > 0 && (
-                <div className="content">
-                    <img src={triangle} alt="triangle" className="leftTriangle"/>
-                    <img src={triangle} alt="triangle" className="rightTriangle"/>
+                <div className="contentFeed">
+                    {restaurants.map((restaurant, index) => (
+                        <Link key={index} to={`/restaurants/${restaurant._id}`}>
+                            <img src={`${API_BASE}/${restaurant.profileImage}`} alt={restaurant.name} className="restaurantImageFeed"/>
+                        </Link>
+                    ))}
                 </div>
             )}
 
-            <h1 className="title">Reviews</h1>
+            <h1 className="titleFeed">Reviews</h1>
             {reviews.length === 0 && (
-                <div className="lastContent">
-                    <h1 className= "noDataReview">AINDA NÃO EXISTEM REVIEWS CADASTRADAS NO SITE</h1>
+                <div className="noLastContentFeed">
+                    <h1 className= "noDataReviewFeed">AINDA NÃO EXISTEM REVIEWS CADASTRADAS NO SITE</h1>
                 </div>
             )}
             {reviews.length > 0 && (
-                <div className="lastContent">
-                    <img src={triangle} alt="triangle" className="leftTriangle"/>
-                    <img src={triangle} alt="triangle" className="rightTriangle"/>
+                <div className="contentFeed">
                 </div>
             )}
-        </div>
-    )
-}
+    </div>
+)}
+        
 
 export default Feed
